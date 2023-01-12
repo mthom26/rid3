654cecb6a5e22a57866a0ab0449bef499872fa95
mthom26 <1337nooblet@protonmail.com>
2023-01-12 19:10:38 +0000

    Add more Actions and Keycodes

--- src/configuration/actions.rs
+++ src/configuration/actions.rs
@@ -31,20 +31,62 @@ impl<'de> Visitor<'de> for KeyCodeVisitor {
         match v {
             "Up" => Ok(KeyCode(XTermKeyCode::Up)),
             "Down" => Ok(KeyCode(XTermKeyCode::Down)),
+            "Left" => Ok(KeyCode(XTermKeyCode::Left)),
+            "Right" => Ok(KeyCode(XTermKeyCode::Right)),
+            "Esc" => Ok(KeyCode(XTermKeyCode::Esc)),
             "Tab" => Ok(KeyCode(XTermKeyCode::Tab)),
+            "Backspace" => Ok(KeyCode(XTermKeyCode::Backspace)),
+            "Enter" => Ok(KeyCode(XTermKeyCode::Enter)),
+            "Home" => Ok(KeyCode(XTermKeyCode::Home)),
+            "End" => Ok(KeyCode(XTermKeyCode::End)),
+            "PageUp" => Ok(KeyCode(XTermKeyCode::PageUp)),
+            "PageDown" => Ok(KeyCode(XTermKeyCode::PageDown)),
+            "BackTab" => Ok(KeyCode(XTermKeyCode::BackTab)),
+            "Delete" => Ok(KeyCode(XTermKeyCode::Delete)),
+            "Insert" => Ok(KeyCode(XTermKeyCode::Insert)),
             c if c.len() == 1 => Ok(KeyCode(XTermKeyCode::Char(c.chars().next().unwrap()))),
-            _ => Err(serde::de::Error::custom("Invalid KeyCode")),
+            // TODO - Handle F1-F12 keys
+            k => Err(serde::de::Error::custom(format!("Invalid KeyCode `{}`", k))),
         }
     }
 }
 
+// Currently different states use the same KeyCode for different actions. Either merge these actions
+// into one in the General Actions section (then the user would only be able to rebind all of them to
+// the same key) or have separate keys for these actions even where one key for multiple actions
+// would be convenient.
+//
+// TODO - Let one KeyCode map to multiple actions
 #[derive(Debug)]
 enum Action {
-    Up,
-    Down,
-    Quit,
-    Back,
+    // General Actions
+    Prev,
+    Next,
+    Quit, // Quit the application
+    Back, // Exit current context (popup, input, etc...) without saving
     SwitchFocus,
+    ToggleLogs,
+    ScreenOne,
+    ScreenTwo,
+    ScreenThree,
+    Help, // Get help popup
+
+    // MainState Actions
+    RemoveFiles,   // Remove files
+    WriteTags,     // Save new tags to files
+    SelectCurrent, // Select currently highlighted item
+    SelectAll,     // Select all highlighted items
+    Remove,        // Remove currently selected files/frames
+    SpawnPopup,
+
+    // FilesState Actions
+    AddAllFiles, // Add all files
+    AddFile,     // Add highlighted file
+    ParentDir,   // Move to parent directory
+    EnterDir,
+
+    // FramesState Actions
+    AddFrame,
 }
 
 impl<'de> Deserialize<'de> for Action {
@@ -70,11 +112,32 @@ impl<'de> Visitor<'de> for ActionVisitor {
         E: serde::de::Error,
     {
         match v {
-            "up" => Ok(Action::Up),
-            "down" => Ok(Action::Down),
+            // General Actions
+            "up" => Ok(Action::Prev),
+            "down" => Ok(Action::Next),
             "quit" => Ok(Action::Quit),
+            "back" => Ok(Action::Back),
             "switch_focus" => Ok(Action::SwitchFocus),
-            _ => Err(serde::de::Error::custom("Invalid action")),
+            "toggle_logs" => Ok(Action::ToggleLogs),
+            "screen_one" => Ok(Action::ScreenOne),
+            "screen_two" => Ok(Action::ScreenTwo),
+            "screen_three" => Ok(Action::ScreenThree),
+            "help" => Ok(Action::Help),
+            // MainState Actions
+            "remove_files" => Ok(Action::RemoveFiles),
+            "write_tags" => Ok(Action::WriteTags),
+            "select_current" => Ok(Action::SelectCurrent),
+            "select_all" => Ok(Action::SelectAll),
+            "remove" => Ok(Action::Remove),
+            "spawn_popup" => Ok(Action::SpawnPopup),
+            // FilesState Actions
+            "add_file" => Ok(Action::AddFile),
+            "add_all_files" => Ok(Action::AddAllFiles),
+            "parent_directory" => Ok(Action::ParentDir),
+            "enter_directory" => Ok(Action::EnterDir),
+            // FramesState Actions
+            "add_frame" => Ok(Action::AddFrame),
+            a => Err(serde::de::Error::custom(format!("Invalid action `{}`", a))),
         }
     }
 }
@@ -106,7 +169,7 @@ impl<'de> Visitor<'de> for ActionMapVisitor {
     {
         let mut values = HashMap::new();
 
-        while let Some((key, value)) = map.next_entry().unwrap() {
+        while let Some((key, value)) = map.next_entry()? {
             // Note the value and key are switched here because we want
             // the `config.toml` entries to read as `Action: Keycode`
             // but the HashMap should have a key of `KeyCode` and value
